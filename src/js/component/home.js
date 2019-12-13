8dd493dae542709d517a3f88c675cab8d194b999
Luis Reyes <luisreyessegner@gmail.com>
2019-12-13 01:09:35 -0300

    Counter Finished

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,52 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+var counter = 0;
+var hrs = 0;
+var min = 0;
+var sec = 0;
+
+export function Home(props) {
+	while (counter < props.seconds) {
+		sec = counter % 60;
+		min = Math.floor((counter / 60) % 60);
+		hrs = Math.floor((counter / 3600) % 24);
+		counter++;
+
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-3 ml-md-auto" />
+					<div className="col-md-3 ml-md-auto">
+						<h2>HOURS:</h2>
+					</div>
+					<div className="col-md-3 ml-md-auto">
+						<h2>MINUTES:</h2>
+					</div>
+					<div className="col-md-3 ml-md-auto">
+						<h2>SECONDS:</h2>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-3 ml-md-auto">
+						<i className="fas fa-stopwatch" />
+					</div>
+					<div className="col-md-3 ml-md-auto">
+						<h1>{hrs}</h1>
+					</div>
+					<div className="col-md-3 ml-md-auto">
+						<h1>{min}</h1>
+					</div>
+					<div className="col-md-3 ml-md-auto">
+						<h1>{sec}</h1>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
+
+Home.propTypes = {
+	seconds: PropTypes.number.isRequired
+};
